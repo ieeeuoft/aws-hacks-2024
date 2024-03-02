a2f10640f788f484aef9569f17541b378d7d2ff7
Thomas <37830237+thomaslin2020@users.noreply.github.com>
2024-03-02 16:21:58 -0500

    Update 0020_alter_application_program.py

--- hackathon_site/registration/migrations/0020_alter_application_program.py
+++ hackathon_site/registration/migrations/0020_alter_application_program.py
@@ -6,13 +6,29 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('registration', '0019_auto_20240301_1145'),
+        ("registration", "0019_auto_20240301_1145"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='application',
-            name='program',
-            field=models.CharField(choices=[(None, ''), ('chemical-engineering', 'Chemical Engineering'), ('civil-engineering', 'Civil Engineering'), ('computer-science', 'Computer Science'), ('electrical-engineering', 'Electrical Engineering'), ('computer-engineering', 'Computer Engineering'), ('engineering-science', 'Engineering Science'), ('industrial-engineering', 'Industrial Engineering'), ('mechanical-engineering', 'Mechanical Engineering'), ('materials-engineering', 'Materials Engineering'), ('mineral-engineering', 'Mineral Engineering'), ('track-one', 'TrackOne')], max_length=50),
+            model_name="application",
+            name="program",
+            field=models.CharField(
+                choices=[
+                    (None, ""),
+                    ("chemical-engineering", "Chemical Engineering"),
+                    ("civil-engineering", "Civil Engineering"),
+                    ("computer-science", "Computer Science"),
+                    ("electrical-engineering", "Electrical Engineering"),
+                    ("computer-engineering", "Computer Engineering"),
+                    ("engineering-science", "Engineering Science"),
+                    ("industrial-engineering", "Industrial Engineering"),
+                    ("mechanical-engineering", "Mechanical Engineering"),
+                    ("materials-engineering", "Materials Engineering"),
+                    ("mineral-engineering", "Mineral Engineering"),
+                    ("track-one", "TrackOne"),
+                ],
+                max_length=50,
+            ),
         ),
     ]
